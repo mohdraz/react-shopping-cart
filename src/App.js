cb3d151d5021f821442ef17ed50f7e5bfac51f6a
mohdraz <noorinfotechinc@gmail.com>
2019-11-12 22:48:28 -0600

    Initating Project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+/* */
 
 // Components
 import Navigation from './components/Navigation';
